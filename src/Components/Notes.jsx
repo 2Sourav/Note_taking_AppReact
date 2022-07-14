d20ee95ddec09225e30d2acc575cf43ea032bbba
Sourav Sen <81104501+2Sourav@users.noreply.github.com>
2022-07-14 16:29:43 +0530

    Initial commit

---
+++ src/Components/Notes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is the React portion</h1>
+      <p>
+        React makes it painless to create interactive UIs. Design simple views
+        for each state in your application, and React will efficiently update
+        and render just the right components when your data changes.
+      </p>
+    </div>
+  );
+}
+export default Notes;
